f43ed98b021345fb0d6e792070c14df93cd4a367
Lucas <lucasnobreoficial@hotmail.com>
2026-03-03 17:32:15 -0300

    feature: Criação do dashboard com gráficos e informações de membros

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,26 @@
-import { UserCircleIcon } from '@heroicons/react/24/outline'
-import React from 'react'
+import { UserCircleIcon } from "@heroicons/react/24/outline";
+import React from "react";
 
-const Card = ({title = "Membros ativos totais", number = "67"}) => {
+const colorVariants = {
+  orange: "w-10 text-amber-400 rounded-2xl",
+  red: "w-10 text-red-400 rounded-2xl",
+  green: "w-10 text-emerald-400 rounded-2xl",
+  purple: "w-10 text-purple-400 rounded-2xl"
+};
+
+const Card = ({
+  title = "Membros ativos totais",
+  number = "67",
+  Icon = "",
+  ColorStandard = "",
+}) => {
   return (
-        <div className='rounded-2xl w-76 bg-linear-to-l from-[#1f1c0162] to-[#53330348]  p-5 flex-col'>
-        <UserCircleIcon className='w-10 text-amber-400 bg-amber-300/35 rounded-2xl'/>
-        <h2 className='text-zinc-300 font-semibold'>{title}</h2>
-        <p className='text-4xl font-bold'>{number}</p>
+    <div className="rounded-2xl w-76 bg-[#1A1A1A]   p-5 flex-col">
+      <Icon className={colorVariants[ColorStandard]} />
+      <h2 className="text-zinc-300 font-semibold">{title}</h2>
+      <p className="text-4xl font-bold">{number}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -1,18 +1,122 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 import Card from "../../components/Card/Card";
+import { api } from "../../services/api";
+import {
+  UsersIcon,
+  ExclamationCircleIcon,
+  CalendarDaysIcon,
+} from "@heroicons/react/24/solid";
 
 const Index = () => {
+  const [error, setError] = useState([]);
+  const [data, setData] = useState([]);
+  const [membersInfo, setMembersInfo] = useState([]);
+
+  useEffect(() => {
+    const getMembers = async () => {
+      try {
+        const res = await api.get("/dashboard");
+        setMembersInfo(res.data);
+        setData(res.data.daily_revenue);
+      } catch (error) {
+        setError("Falha ao buscar usuários", error);
+      }
+    };
+
+    getMembers();
+  }, []);
+
+  console.log(membersInfo);
+
   return (
     <div className="text-white w-full h-screen flex justify-center items-center">
-      <div className="m-5 w-full p-5 h-full">
+      <div className="m-5 w-full p-5 h-full flex flex-col gap-3">
         <h1 className="text-white text-4xl font-bold">Dashboard Visão Geral</h1>
-        <p className="text-gray-200 font-semibold">Monitore a performace de sua empresa e o ciclo de vida dos clientes ativos</p>
+        <p className="text-gray-200 font-semibold">
+          Monitore a performace de sua empresa e o ciclo de vida dos clientes
+          ativos
+        </p>
         <div className="mt-5 flex gap-5">
-          <Card/>
-          <Card/>
-          <Card/>
+          <Card
+            number={membersInfo?.active_members}
+            title={"Membros Ativos"}
+            Icon={UsersIcon}
+            ColorStandard={"green"}
+          />
+          <Card
+            number={membersInfo?.overdue_members}
+            title={"Membros Inadimplentes"}
+            Icon={ExclamationCircleIcon}
+            ColorStandard={"red"}
+          />
+          <Card
+            number={membersInfo?.members_due_today}
+            title={"Vencendos Hoje"}
+            Icon={CalendarDaysIcon}
+            ColorStandard={"purple"}
+          />
+          <Card
+            number={`R$ ${membersInfo?.monthly_revenue}`}
+            title={"Lucro Bruto Mensal"}
+            Icon={CalendarDaysIcon}
+            ColorStandard={"orange"}
+          />
         </div>
-
+        <div className="w-full h-120 p-5 bg-[#1A1A1A] rounded-2xl mt-2">
+          <div className="flex flex-col items-start justify-start ml-8 pt-2">
+            <h1 className="text-gray-200 text-4xl font-bold">
+              Relação de pagamentos
+            </h1>
+            <p className="text-zinc-500 mb-2 font-semibold">
+              Pagamentos recebidos em cada dia do mês
+            </p>
+          </div>
+          <ResponsiveContainer width="98%" height="75%">
+            <LineChart data={data}>
+              <CartesianGrid stroke="#333" strokeDasharray="3 3" />
+              <XAxis
+                dataKey="day"
+                label={{
+                  value: "Dias do mês atual",
+                  position: "insideBottom",
+                  offset: -5,
+                  fill: "#ccc",
+                }}
+                stroke="#ccc"
+              />
+              <YAxis
+                stroke="#ccc"
+                label={{
+                  value: "Valores recebidos",
+                  angle: -90,
+                  position: "insideLeft",
+                  fill: "#ccc",
+                }}
+              />
+              <Tooltip
+                contentStyle={{ backgroundColor: "#222", borderRadius: "6px" }}
+                labelStyle={{ color: "#fff" }}
+                itemStyle={{ color: "#3472b1" }}
+              />
+              <Line
+                type="monotone"
+                dataKey="revenue"
+                stroke="#1E90FF"
+                strokeWidth={3}
+                dot={{ r: 4 }}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
       </div>
     </div>
   );

bc13d62c4434a2cff6eeb3531cec44d78e92cd10
Lucas <lucasnobreoficial@hotmail.com>
2026-03-11 20:37:29 -0300

    feature: implementação de filtros e pesquisa na tabela

--- src/pages/Members/components/MembersTable.jsx
+++ src/pages/Members/components/MembersTable.jsx
@@ -1,90 +1,80 @@
 import { PencilIcon, TrashIcon } from "@heroicons/react/24/outline";
 import { Link, NavLink } from "react-router-dom";
-import React, { useState } from "react";
-import { api } from "../../../services/api";
 
-const MembersTable = ({ members, attMembers }) => {
-  const [error, setError] = useState(false);
-
-  const deleteMember = async (id) => {
-    try {
-      await api.delete(`/members/${id}`);
-      attMembers((prev) => prev.filter((member) => member.id !== id));
-    } catch {
-      setError("Erro ao deletar usuário");
-    }
-  };
-
+const MembersTable = ({ members, deleteMember, search, setSearch, handleSort }) => {
   return (
-    <table className="bg-[#262626]">
-      <thead className="bg-[#151515] text-left text-sm font-semibold">
-        <tr>
-          <th className="px-4 py-3 font-bold text-gray-600/74 text-center">
-            Nome
-          </th>
-          <th className="px-4 py-3 font-bold text-gray-600/74 text-center">
-            Email
-          </th>
-          <th className="px-4 py-3 font-bold text-gray-600/74 text-center">
-            Plano
-          </th>
-          <th className="px-4 py-3 font-bold text-gray-600/74 text-center">
-            Status
-          </th>
-          <th className="px-4 py-3 font-bold text-gray-600/74 text-center">
-            Dia de vencimento
-          </th>
-          <th className="px-4 py-3 font-bold text-gray-600/74 text-center">
-            Ações
-          </th>
-        </tr>
-      </thead>
+    <div className="bg-[#5c471f21] w-[70%] p-5 border border-zinc-800 rounded-2xl">
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="p-2 my-2 w-[50%] bg-[#262626] rounded border-zinc-800 border text-zinc-400 font-semibold"
+          placeholder="Pesquise o membro"
+        />
 
-      <tbody className="divide-y">
-        {members.map((member) => {
-          return (
+        
+      </form>
+
+      <table className="bg-[#262626] w-full">
+        <thead className="bg-[#151515] text-sm font-semibold">
+          <tr>
+            <th className="px-4 py-3 text-center text-gray-600/74 cursor-pointer" onClick={() => handleSort("name")} >Nome</th>
+            <th className="px-4 py-3 text-center text-gray-600/74 cursor-pointer" onClick={() => handleSort("email")} >Email</th>
+            <th className="px-4 py-3 text-center text-gray-600/74 cursor-pointer" onClick={() => handleSort("plan")} >Plano</th>
+            <th className="px-4 py-3 text-center text-gray-600/74 cursor-pointer"  onClick={() => handleSort("status")}>Status</th>
+            <th className="px-4 py-3 text-center text-gray-600/74 cursor-pointer" onClick={() => handleSort("due_day")}> 
+              Dia de vencimento
+            </th>
+            <th className="px-4 py-3 text-center text-gray-600/74">Ações</th>
+          </tr>
+        </thead>
+
+        <tbody className="divide-y">
+          {members.map((member) => (
             <tr
               key={member.id}
-              className="hover:bg-[#202020] transition-colors cursor-pointer"
+              className="hover:bg-[#202020] transition-colors"
             >
-              <td className="px-4 py-3 font-medium ">
-                <NavLink to={`/members/page/${member.id}`} className="text-center font-extrabold capitalize text-white">
+              <td className="px-4 py-3">
+                <NavLink
+                  to={`/members/page/${member.id}`}
+                  className="font-extrabold capitalize text-white"
+                >
                   {member.name}
                 </NavLink>
               </td>
-              <td className="px-4 py-3 font-medium ">
-                <p className="text-gray-400/50 font-semibold text-center">
-                  {member.email}
-                </p>
+
+              <td className="px-4 py-3 text-center text-gray-400">
+                {member.email}
               </td>
-              <td className="px-4 py-3 font-medium ">
-                <p className="text-gray-400/50 font-semibold text-center">
-                  {member.plan.name}
-                </p>
+
+              <td className="px-4 py-3 text-center text-gray-400">
+                {member.plan?.name}
               </td>
-              <td className="px-4 py-3 font-medium ">
-                <p className="text-gray-400/50 font-semibold text-center">
-                  {member.status}
-                </p>
+
+              <td className="px-4 py-3 text-center text-gray-400">
+                {member.status}
               </td>
-              <td className="px-4 py-3 font-medium">
-                <p className="text-gray-400/50 font-semibold text-center">
-                  {member.due_day}
-                </p>
+
+              <td className="px-4 py-3 text-center text-gray-400">
+                {member.due_day}
               </td>
-              <td className="px-4 py-3 font-medium flex gap-2">
+
+              <td className="px-4 py-3 flex justify-center gap-2">
                 <button onClick={() => deleteMember(member.id)}>
-                  <TrashIcon className="w-6 text-gray-400 cursor-pointer hover:text-gray-600" />
+                  <TrashIcon className="w-6 text-gray-400 hover:text-gray-600" />
                 </button>
+
                 <Link to={`/members/edit/${member.id}`}>
-                  <PencilIcon className="w-6 text-gray-400 cursor-pointer hover:text-gray-600" />
+                  <PencilIcon className="w-6 text-gray-400 hover:text-gray-600" />
                 </Link>
               </td>
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
--- src/pages/Members/pages/MemberList.jsx
+++ src/pages/Members/pages/MemberList.jsx
@@ -1,25 +1,35 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../../services/api";
 import MembersTable from "../components/MembersTable";
-import UserTable from "../../UserTable/UserTable";
+import Loading from "../../../components/Loading";
 
 const MemberList = () => {
   const [members, setMembers] = useState([]);
-  const [countMember, setCountMember] = useState(67);
+  const [countMember, setCountMember] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
-  const [lastpage, setLastPage] = useState(1);
+  const [lastPage, setLastPage] = useState(1);
   const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState("");
+  const [sortBy, setSortBy] = useState(null);
+  const [direction, setDirection] = useState("asc");
 
-  const loadMembers = async (page = 1) => {
+  const loadMembers = async (page = 1, searchValue = search) => {
     try {
       setLoading(true);
 
-      const res = await api.get(`/customers?page=${page}`);
+      const res = await api.get("/members", {
+        params: {
+          page: page,
+          search: searchValue,
+          sort_by: sortBy,
+          direction: direction
+        },
+      });
 
       setMembers(res.data.data);
       setCurrentPage(res.data.meta.current_page);
       setLastPage(res.data.meta.last_page);
-      setCountMember(res.data.meta.total)
+      setCountMember(res.data.meta.total);
 
     } catch (err) {
       console.error(err);
@@ -28,55 +38,91 @@ const MemberList = () => {
     }
   };
 
+  const handleSort = (column) => {
+    if (sortBy === column) {
+      setDirection(direction === "asc" ? "desc" : "asc");
+    } else {
+      setSortBy(column);
+      setDirection("asc");
+    }
+  }
+
+  useEffect(() => {
+    const delay = setTimeout(() => {
+      loadMembers(1);
+      setCurrentPage(1);
+    }, 400);
+
+    return () => clearTimeout(delay);
+  }, [search]);
+
   useEffect(() => {
     loadMembers(currentPage);
-  }, [currentPage]);
+  }, [currentPage, sortBy, direction]);
+
+  const deleteMember = async (id) => {
+    try {
+      await api.delete(`/members/${id}`);
+      loadMembers(currentPage);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   const nextPage = () => {
-    if (currentPage < lastpage) {
-      setCurrentPage(currentPage + 1);
+    if (currentPage < lastPage) {
+      setCurrentPage((prev) => prev + 1);
     }
   };
 
   const prevPage = () => {
     if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
+      setCurrentPage((prev) => prev - 1);
     }
   };
 
   if (loading) {
-    return (
-      <p className="text-4xl m-auto text-white font-bold">Carregando...</p>
-    );
+    return <Loading />;
   }
 
   return (
     <div>
-      <div>
-        <div className="p-2 mb-8 flex flex-col gap-2">
-          <p className="text-white text-2xl font-bold">Membros na base</p>
-          <p className="text-gray-400 font-semibold text">Atualmente temos {countMember} registrados na academia.</p>
-        </div>
-        <MembersTable members={members} attMembers={setMembers}/>
-        <div className="mt-20">
-          <button
-            onClick={prevPage}
-            disabled={currentPage === 1}
-            className="text-white"
-          >
-            Anterior
-          </button>
-          <span className="mx-10 text-gray-400">
-            Página {currentPage} de {lastpage}
-          </span>
-          <button
-            onClick={nextPage}
-            disabled={currentPage === lastpage}
-            className="text-white"
-          >
-            Próxima
-          </button>
-        </div>
+      <div className="p-2 mb-8 flex flex-col gap-2">
+        <p className="text-white text-2xl font-bold">Membros na base</p>
+
+        <p className="text-gray-400 font-semibold">
+          Atualmente temos {countMember} registrados na academia.
+        </p>
+      </div>
+
+      <MembersTable
+        members={members}
+        deleteMember={deleteMember}
+        search={search}
+        setSearch={setSearch}
+        handleSort={handleSort}
+      />
+
+      <div className="mt-5">
+        <button
+          onClick={prevPage}
+          disabled={currentPage === 1}
+          className="text-white"
+        >
+          Anterior
+        </button>
+
+        <span className="mx-10 text-gray-400">
+          Página {currentPage} de {lastPage}
+        </span>
+
+        <button
+          onClick={nextPage}
+          disabled={currentPage === lastPage}
+          className="text-white"
+        >
+          Próxima
+        </button>
       </div>
     </div>
   );
